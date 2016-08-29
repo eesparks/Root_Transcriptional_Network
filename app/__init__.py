ca9dc1eac9237efcca25daa717e4442c4a7f3410
Gina Turco <gturco88@gmail.com>
2016-08-29 11:45:15 -0700

    no longer need password

--- app/__init__.py
+++ app/__init__.py
@@ -28,47 +28,38 @@ def requires_auth(f):
     return decorated
 
 @app.route('/')
-@requires_auth
 def home():
     return redirect("/index.html", code=302)
 
 @app.route('/index.html')
-@requires_auth
 def index():
     return app.send_static_file('index.html')
 
 @app.route('/2_TF_Clusters.html')
-@requires_auth
 def tf_clusters():
     return app.send_static_file('2_TF_Clusters.html')
 
 @app.route('/3_Motifs_Network_Supp.html')
-@requires_auth
 def motifs():
     return app.send_static_file('3_Motifs_Network_Supp.html')
 
 @app.route('/4_SHR_All.html')
-@requires_auth
 def shr():
     return app.send_static_file('4_SHR_All.html')
 
 
 @app.route('/5_SCR_All.html')
-@requires_auth
 def scr():
     return app.send_static_file('5_SCR_All.html')
 
 @app.route('/6_SHR_Validated.html')
-@requires_auth
 def shr_v():
     return app.send_static_file('6_SHR_Validated.html')
 
 @app.route('/7_SCR_Validated.html')
-@requires_auth
 def scr_v():
     return app.send_static_file('7_SCR_Validated.html')
 
 @app.route('/8_Light.html')
-@requires_auth
 def light():
     return app.send_static_file('8_Light.html')
